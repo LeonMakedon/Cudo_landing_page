849eda7f4c1a3a3e91b43a20e85cedf334d2cef7
Firebaba <ntbilieli@gmail.com>
2021-01-18 22:08:44 +0200

    First commit

---
+++ homework_16/js/script.js
@@ -0,0 +1,41 @@
+'use strict';
+
+window.addEventListener('hashchange', () => {
+    const $menuItems = document.querySelectorAll('.nav-link');
+    const $activeMenuItem = document.querySelector(`[href="${location.hash}"]`);
+
+    $menuItems.forEach(e => e.classList.remove('is-active'));
+    $activeMenuItem.classList.add('is-active');
+});
+
+document.querySelectorAll('[data-filter]')
+    .forEach(tab => {
+        tab.addEventListener('click', (e) => {
+            const type = e.target.dataset.filter;
+            const $filterContent = document.querySelector('.filter-content');
+
+            if (type === 'all') {
+                $filterContent.dataset.hide = '';
+            } else {
+                $filterContent.dataset.hide = type;
+            }
+        });
+    });
+
+document.forms['contact-form'].addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const {
+        your_name: { value: name },
+        your_email: { vakue: email },
+        your_message: { value: message }
+    } = e.target.elements;
+
+    const data = `
+    Your Name: ${name}
+    Your Email: ${email}
+    Your Message: ${message}
+  `;
+
+    alert(data);
+});
